528fab5823bbc63c6685511cf0dcf01b9e43c663
thezoot3 <thezoot3@gmail.com>
2023-07-25 20:37:25 +0900

    removed

--- src/types/api/ClassTimetable.d.ts
+++
@@ -1,9 +0,0 @@
-import Timetable = require('comcigan-parser');
-export default class extends Timetable {
-    classDuration: number[];
-    _schoolName: string;
-    constructor();
-    getClassTimetable(grade: number, classNumber: number, weekDay: Array<number>): Promise<TimetableByWeekday>;
-    getPeriods(periods: number): Promise<PeriodData>;
-    _reset(): Promise<void>;
-}
