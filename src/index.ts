51d51902c7e9f005f42b20f00f80304e2930e5a8
thezoot3 <thezoot3@gmail.com>
2023-08-07 17:41:35 +0900

    commit

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import { initSocket, lockerSocketMessageType } from './lockerSocket'
 import { WebSocket } from 'ws'
 import { getLockerData, LockerData, lockerDataProperties, scanLockerData } from './api/LockerDB'
 import morgan from 'morgan'
+import Timetable from './api/ClassTimetable'
 
 const app = express()
 const server = createServer(app)
 const wss = initSocket(server)
 const lockerClient = new Map<string, WebSocket>()
+const timeTable = new Timetable()
 
 app.use(morgan('dev'))
 app.use(express.json())
@@ -83,4 +85,4 @@ app.put('/api/orderLocker/:uuid', async (req, res) => {
 
 server.listen(80)
 
-export { wss as lockerWss, lockerClient }
+export { wss as lockerWss, lockerClient, timeTable }
